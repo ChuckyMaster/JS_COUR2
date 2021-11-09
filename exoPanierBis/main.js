03a7fe5c0cffca94ccf8b386606c96c0f5a0c779
Chucky Master <user.email>
2021-11-09 01:39:28 +0100

    panier Bis

--- exoPanierBis/main.js
+++ exoPanierBis/main.js
@@ -46,12 +46,11 @@ function recupValue() {
   bask.quantity = document.querySelector("#number").value;
 
   // créer une variable qui appelle la fonction isDoublon
-  let doublon = isDoublon(basket);
+  let doublon = isDoublon(bask); // !! BIEN METTRE EN PARAMETRE LE NEW OBJET DE LA FONCTION ( car valeur reutiliser à l'appelle de la fonction)
   if (doublon == false) {
     basket.push(bask);
   }
   displayBasket();
-  form.reset();
 }
 
 //Verifie les doublons, s'il ya doublons on push la quantité sur le même index, sinon on push sur le tableau global "basket"
@@ -61,7 +60,7 @@ function isDoublon(product) {
   let doublon = false;
   //on boucle sur le tableau basket afin de verifié les doublon
   basket.forEach((line, index) => {
-    if (line.name === product.name) {
+    if (line.name == product.name) {
       //si nous trouvons un doublons nous incrementons la quantité du produit à la même index
       basket[index].quantity =
         parseInt(line.quantity) + parseInt(product.quantity);
@@ -74,27 +73,50 @@ function isDoublon(product) {
 //Afficher le contenu du tableau basket après avoir recupérer les données de la fonction de récupération
 
 function displayBasket() {
-  let li = document.createElement("li");
+  //cibler la div qui affichera la list basket
+  let dispBasket = document.querySelector("#display-basket");
   let ul = document.createElement("ul");
-  let button = document.createElement("button");
-  let icone = document.createElement("i");
+
   //ajoute ul sur la div display basket
   let index = 0;
-
+  //DEBUT BOUCLE FOREACH
   basket.forEach((elem) => {
-    button.appendChild(icone);
+    //Creer les element dans la boucle afin qu'elle se regenere a chaque passage
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    //nous sommes sur le button supprimer, de ce fait j'ajoute la classe .remove
+    button.classList.add("remove"); //!!ATTENTION NE PAS METTRE LE POINT!!!!!
+    let icone = document.createElement("i");
+
     icone.innerHTML = ` <i data-index=${index} class="fas fa-trash "></i> `;
     li.innerHTML = ` ${elem.quantity} ${elem.name}(s) `;
     li.appendChild(button);
+    button.appendChild(icone);
     ul.appendChild(li);
     index++;
   });
 
-  basket.innerHTML = "";
+  //vider le contenu
+  dispBasket.innerHTML = "";
   dispBasket.appendChild(ul);
+
+  //Je stock tout les button avec la classe remove afin d'ajouter un gestionnaire d'evenement au "click" sur chacun d'entre eux
+
+  let remove = document.querySelectorAll(".remove");
+
+  //ajouter le gestionnaire d'evenement sur chaque button .remove qui appelle la fonction removeOne
+  remove.forEach((elem) => {
+    elem.addEventListener("click", removeOne);
+  });
 }
 
 // supprimer un element par ligne du tableau basket
+
+function removeOne(e) {
+  basket.splice(e.target.dataset.index, 1);
+  //event.target permet de pointer sur l'origine de l'evenement, ici la fonction est appellé sur l'evenement au click sur les button . remove
+  displayBasket();
+}
 
 //supprimer un element par son nom
 
@@ -109,7 +131,6 @@ document.addEventListener("DOMContentLoaded", function () {
   add = document.querySelector("#add");
   produit = document.querySelector("#produits");
   quantity = document.querySelector("#number");
-  dispBasket = document.querySelector("#display-basket");
 
   /*
    **************EVENT************
@@ -121,6 +142,4 @@ document.addEventListener("DOMContentLoaded", function () {
     return;
   });
   add.addEventListener("click", recupValue);
-
-  /**create Element */
 });
